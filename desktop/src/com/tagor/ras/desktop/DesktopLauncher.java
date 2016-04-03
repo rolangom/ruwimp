77ef9adca8057ed2710385fdcb90c15b7456d778
Rolando Gómez Tabar <rolangom@gmail.com>
2016-04-02 22:54:56 -0400

    Desktop config changes

--- desktop/src/com/tagor/ras/desktop/DesktopLauncher.java
+++ desktop/src/com/tagor/ras/desktop/DesktopLauncher.java
@@ -1,4 +1,4 @@
-package com.tagor.rumper.desktop;
+package com.tagor.ras.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -7,8 +7,6 @@ import com.tagor.ras.RasGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 480;
 		new LwjglApplication(new RasGame(), config);
 	}
 }
